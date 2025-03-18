98aab87541679f79ba471d5bea608508e773ff17
francisvimal2002 <francisvimal2002@gmail.com>
2025-03-18 09:11:10 +0000

    002 - Added audio and key press to each image

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 var numberOfDrums = document.querySelectorAll(".drum");
 for(var i = 0; i < numberOfDrums.length; i++) {
-    numberOfDrums[i].addEventListener("click", clickFunction);
-}
+    numberOfDrums[i].addEventListener("click", function () {
+        drumSounds(this.innerHTML);
+    });
+};
 
-function clickFunction() {
-    alert("I'm still working");
-}
+document.addEventListener("keydown", function (event) {
+    drumSounds(event.key);
+})
+
+function drumSounds(key) {
+    switch(key) {
+        case "w":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        default:
+            break;
+    }
+};
